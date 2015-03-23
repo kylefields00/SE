ca3d742ff642535ad5b0abd566b808dcd8343ed6
kylefields00 <kylefields85@gmail.com>
2015-03-22 18:36:55 -0600

    Update SE1.py

--- SE1.py
+++ SE1.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Mar  7 20:01:49 2015
 
-@author: gridxkl
+@author: kylefields00
 """
 
 import requests
@@ -17,3 +17,20 @@ def parseText(html):
     bs = BeautifulSoup(html)
     return bs.select('div.article')[0].text
     
+class Crawler(object): 
+    def __init__(self, start_url):
+        self.start_url = start_url
+        
+    @staticmethod
+    def _make_absolute_url(url):
+        return 'http://reddit.com' + url
+        
+    def crawl(self):
+        current_page_url = self.start_url
+        while True:
+            
+        current_page = downloadUrl(current_page_url)
+        bs = BeautifulSoup(current_page)
+        all_posts_links = bs.findAll('a',attrs={'class':'title'});  
+        post_links = [Crawler._make_absolute_url(link['href']) for link in all_posts_links]
+        
